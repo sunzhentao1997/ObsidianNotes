cdcde06c32c54efc8d0632655de192def914c6d9
sunzhentao <1575494860@qq.com>
2025-07-25 13:42:13 +0800

    vault backup: 2025-07-25 13:42:13

---
+++ 单片机学习笔记/FlashDB/FlashDB_Demo_F103/Core/Inc/flashdb_demo.h
@@ -0,0 +1,14 @@
+/*
+ * flashdb_demo.h
+ *
+ *  Created on: Jul 25, 2025
+ *      Author: szt
+ */
+
+#ifndef INC_FLASHDB_DEMO_H_
+#define INC_FLASHDB_DEMO_H_
+
+uint8_t flashdb_kvdb_init(void);
+void flashdb_kvdb_main(void);
+
+#endif /* INC_FLASHDB_DEMO_H_ */
